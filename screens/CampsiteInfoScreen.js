0d7849827a882501e77ac8b5fea6eb0057f754ad
Slone <slone.christopher@gmail.com>
2023-02-25 11:05:25 -0500

    testing update

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,105 @@
+import { FlatList, StyleSheet, Text, Button, Modal, View  } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import RenderCampsite from '../features/campsites/RenderCampsite';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
+import { useState } from 'react';
+
+function setShowModal () {
+const showModal = useState(false);
+};
+
+const CampsiteInfoScreen = ({ route }) => {
+    const { campsite } = route.params;
+    const comments = useSelector((state) => state.comments);
+    const favorites = useSelector((state) => state.favorites);
+    const dispatch = useDispatch();
+
+    const renderCommentItem = ({ item }) => {
+        return (
+            <View style={styles.commentItem}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>
+                    {`-- ${item.author}, ${item.date}`}
+                </Text>
+            </View>
+        );
+    };
+
+    return (
+        <>
+        <FlatList
+            data={comments.commentsArray.filter(
+                (comment) => comment.campsiteId === campsite.id
+            )}
+            renderItem={renderCommentItem}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{
+                marginHorizontal: 20,
+                paddingVertical: 20
+            }}
+            ListHeaderComponent={
+                <>
+                    <RenderCampsite
+                        campsite={campsite}
+                        isFavorite={favorites.includes(campsite.id)}
+                        markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                        onShowModal={() => setShowModal(!showModal)}
+                    />
+                    <Text style={styles.commentsTitle}>Comments</Text>
+                </>
+            }
+        />
+        <Modal
+                animationType='slide'
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+                <View style={styles.modal}>
+                    <Button
+                        onPress={() => {
+                            setShowModal(!showModal);
+                            resetForm();
+                        }}
+                        color='#5637DD'
+                        title='Close'
+                    />
+                    <View style={{margin: 10}}>
+                        <Button
+                            onPress={() => {
+                                setShowModal(!showModal);
+                                resetForm();
+                            }}
+                            color='#808080'
+                            title='Cancel'
+                        />
+                    </View>
+                </View>
+        </Modal>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    commentsTitle: {
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#43484D',
+        padding: 10,
+        paddingTop: 30
+    },
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#fff'
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    }
+});
+
+export default CampsiteInfoScreen;
---
+++ screens/ContactScreen.js
@@ -0,0 +1,23 @@
+import { ScrollView, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+const ContactScreen = () => {
+
+    return (
+        <ScrollView>
+            <Card wrapperStyle={{ margin: 10 }}>
+                <Card.Title>
+                    Contact Information 
+                </Card.Title>
+                <Card.Divider />
+                <Text>1 Numcamp Way</Text>
+                <Text>Seatlle, WA 98001</Text>
+                <Text style={{ marginBottom: 10 }}>U.S.A</Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+            </Card>
+        </ScrollView>
+    );
+};
+
+export default ContactScreen;
